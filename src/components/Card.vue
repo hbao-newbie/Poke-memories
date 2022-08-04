538f501cbaf8168d5fe4591bacb5143d72f3426e
hbao-newbie <baob1906432@student.ctu.edu.vn>
2022-08-04 23:21:46 +0700

    HI

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -25,7 +25,7 @@
             <div class="card__face card__face--back">
                 <div 
                     class="card__content"
-                    :style="`url('/src/assets/${imgBackFaceUrl}')`"
+                    :style="{ backgroundImage: `url('/src/assets/${imgBackFaceUrl}')` }"
                 ></div>
             </div>
         </div>
